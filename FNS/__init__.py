2dab301bfd41ada3353ab510ba4d2fd0b6683b77
YiboW <yibocat@yeah.net>
2022-10-07 23:58:34 +0800

    update FNS

--- FNS/__init__.py
+++ FNS/__init__.py
@@ -8,6 +8,8 @@ __all__ = ['IFN',
            'IFN_Algebraic_Plus',
            'IFN_Einstein_Multiply',
            'IFN_Einstein_Plus',
+           'IFN_Intersection',
+           'IFN_Union',
            'IVIFN_Algebraic_Multiply',
            'IVIFN_Algebraic_Plus',
            'IVIFN_Einstein_Multiply',
@@ -16,6 +18,8 @@ __all__ = ['IFN',
            'PFN_Algebraic_Plus',
            'PFN_Einstein_Multiply',
            'PFN_Einstein_Plus',
+           'PFN_Intersection',
+           'PFN_Union',
            'IVPFN_Algebraic_Multiply',
            'IVPFN_Algebraic_Plus',
            'IVPFN_Einstein_Multiply',
@@ -24,20 +28,24 @@ __all__ = ['IFN',
            'FFN_Algebraic_Plus',
            'FFN_Einstein_Multiply',
            'FFN_Einstein_Plus',
+           'FFN_Intersection',
+           'FFN_Union',
            'IVFFN_Algebraic_Multiply',
            'IVFFN_Algebraic_Plus',
            'IVFFN_Einstein_Multiply',
            'IVFFN_Einstein_Plus',
            ]
 
-from FNS.FNs import (IFN,IVIFN,PFN,IVPFN,FFN,IVFFN)
-from FNS.FNsOperations import (IFN_Algebraic_Multiply,IFN_Algebraic_Plus,IFN_Einstein_Multiply,IFN_Einstein_Plus,
-                            IVIFN_Algebraic_Multiply,IVIFN_Algebraic_Plus,IVIFN_Einstein_Multiply,IVIFN_Einstein_Plus,
-                            PFN_Algebraic_Multiply,PFN_Algebraic_Plus,PFN_Einstein_Multiply,PFN_Einstein_Plus,
-                            IVPFN_Algebraic_Multiply,IVPFN_Algebraic_Plus,IVPFN_Einstein_Multiply,IVPFN_Einstein_Plus,
-                            FFN_Algebraic_Multiply,FFN_Algebraic_Plus,FFN_Einstein_Multiply,FFN_Einstein_Plus,
-                            IVFFN_Algebraic_Multiply,IVFFN_Algebraic_Plus,IVFFN_Einstein_Multiply,IVFFN_Einstein_Plus
-                            )
+
+
+from FNS.IFN import (IFN,IFN_Intersection,IFN_Union,IFN_Algebraic_Multiply,IFN_Algebraic_Plus,IFN_Einstein_Multiply,IFN_Einstein_Plus)
+from FNS.PFN import (PFN,PFN_Intersection,PFN_Union,PFN_Algebraic_Multiply,PFN_Algebraic_Plus,PFN_Einstein_Multiply,PFN_Einstein_Plus)
+from FNS.FFN import (FFN,FFN_Intersection,FFN_Union,FFN_Algebraic_Multiply,FFN_Algebraic_Plus,FFN_Einstein_Multiply,FFN_Einstein_Plus)
+from FNS.IVIFN import (IVIFN,IVIFN_Algebraic_Multiply,IVIFN_Algebraic_Plus,IVIFN_Einstein_Multiply,IVIFN_Einstein_Plus)
+from FNS.IVPFN import (IVPFN,IVPFN_Algebraic_Multiply,IVPFN_Algebraic_Plus,IVPFN_Einstein_Multiply,IVPFN_Einstein_Plus)
+from FNS.IVFFN import (IVFFN,IVFFN_Algebraic_Multiply,IVFFN_Algebraic_Plus,IVFFN_Einstein_Multiply,IVFFN_Einstein_Plus)
+
+
 
 __all__ += ['algebraic_tau','in_algebraic_tau','algebraic_s','in_algebraic_s',
             'algebraic_T','algebraic_S','pithy_algebraic_T','pithy_algebraic_S',
@@ -49,41 +57,44 @@ from FNS.Archimedean import (algebraic_tau,in_algebraic_tau,algebraic_s,in_algeb
                         einstein_tau,in_einstein_tau,einstein_s,in_einstein_s,
                         einstein_T,einstein_S,pithy_einstein_T,pithy_einstein_S)
 
-__all__ += ['InteractionIFN',
-            'InteractionIVIFN',
-            'InteractionPFN',
-            'InteractionIVPFN',
-            'InteractionFFN',
-            'InteractionIVFFN',
-            'Interaction_IFN_Algebraic_Multiply',
-            'Interaction_IFN_Algebraic_Plus',
-            'Interaction_IFN_Einstein_Multiply',
-            'Interaction_IFN_Einstein_Plus',
-            'Interaction_IVIFN_Algebraic_Multiply',
-            'Interaction_IVIFN_Algebraic_Plus',
-            'Interaction_IVIFN_Einstein_Multiply',
-            'Interaction_IVIFN_Einstein_Plus',
-            'Interaction_PFN_Algebraic_Multiply',
-            'Interaction_PFN_Algebraic_Plus',
-            'Interaction_PFN_Einstein_Multiply',
-            'Interaction_PFN_Einstein_Plus',
-            'Interaction_IVPFN_Algebraic_Multiply',
-            'Interaction_IVPFN_Algebraic_Plus',
-            'Interaction_IVPFN_Einstein_Multiply',
-            'Interaction_IVPFN_Einstein_Plus',
-            'Interaction_FFN_Algebraic_Multiply',
-            'Interaction_FFN_Algebraic_Plus',
-            'Interaction_FFN_Einstein_Multiply',
-            'Interaction_FFN_Einstein_Plus',
-            'Interaction_IVFFN_Algebraic_Multiply',
-            'Interaction_IVFFN_Algebraic_Plus',
-            'Interaction_IVFFN_Einstein_Multiply',
-            'Interaction_IVFFN_Einstein_Plus']
 
-from FNS.InteractionFNs import (InteractionIFN,InteractionIVIFN,InteractionPFN,InteractionIVPFN,InteractionFFN,InteractionIVFFN,
-                                Interaction_IFN_Algebraic_Multiply,Interaction_IFN_Algebraic_Plus,Interaction_IFN_Einstein_Multiply,Interaction_IFN_Einstein_Plus,
-                                Interaction_IVIFN_Algebraic_Multiply,Interaction_IVIFN_Algebraic_Plus,Interaction_IVIFN_Einstein_Multiply,Interaction_IVIFN_Einstein_Plus,
-                                Interaction_PFN_Algebraic_Multiply,Interaction_PFN_Algebraic_Plus,Interaction_PFN_Einstein_Multiply,Interaction_PFN_Einstein_Plus,
-                                Interaction_IVPFN_Algebraic_Multiply,Interaction_IVPFN_Algebraic_Plus,Interaction_IVPFN_Einstein_Multiply,Interaction_IVPFN_Einstein_Plus,
-                                Interaction_FFN_Algebraic_Multiply,Interaction_FFN_Algebraic_Plus,Interaction_FFN_Einstein_Multiply,Interaction_FFN_Einstein_Plus,
-                                Interaction_IVFFN_Algebraic_Multiply,Interaction_IVFFN_Algebraic_Plus,Interaction_IVFFN_Einstein_Multiply,Interaction_IVFFN_Einstein_Plus,)
+
+### Interaction 还有些问题，需要修补
+# __all__ += ['InteractionIFN',
+#             'InteractionIVIFN',
+#             'InteractionPFN',
+#             'InteractionIVPFN',
+#             'InteractionFFN',
+#             'InteractionIVFFN',
+#             'Interaction_IFN_Algebraic_Multiply',
+#             'Interaction_IFN_Algebraic_Plus',
+#             'Interaction_IFN_Einstein_Multiply',
+#             'Interaction_IFN_Einstein_Plus',
+#             'Interaction_IVIFN_Algebraic_Multiply',
+#             'Interaction_IVIFN_Algebraic_Plus',
+#             'Interaction_IVIFN_Einstein_Multiply',
+#             'Interaction_IVIFN_Einstein_Plus',
+#             'Interaction_PFN_Algebraic_Multiply',
+#             'Interaction_PFN_Algebraic_Plus',
+#             'Interaction_PFN_Einstein_Multiply',
+#             'Interaction_PFN_Einstein_Plus',
+#             'Interaction_IVPFN_Algebraic_Multiply',
+#             'Interaction_IVPFN_Algebraic_Plus',
+#             'Interaction_IVPFN_Einstein_Multiply',
+#             'Interaction_IVPFN_Einstein_Plus',
+#             'Interaction_FFN_Algebraic_Multiply',
+#             'Interaction_FFN_Algebraic_Plus',
+#             'Interaction_FFN_Einstein_Multiply',
+#             'Interaction_FFN_Einstein_Plus',
+#             'Interaction_IVFFN_Algebraic_Multiply',
+#             'Interaction_IVFFN_Algebraic_Plus',
+#             'Interaction_IVFFN_Einstein_Multiply',
+#             'Interaction_IVFFN_Einstein_Plus']
+
+# from FNS.InteractionFNs import (InteractionIFN,InteractionIVIFN,InteractionPFN,InteractionIVPFN,InteractionFFN,InteractionIVFFN,
+#                                 Interaction_IFN_Algebraic_Multiply,Interaction_IFN_Algebraic_Plus,Interaction_IFN_Einstein_Multiply,Interaction_IFN_Einstein_Plus,
+#                                 Interaction_IVIFN_Algebraic_Multiply,Interaction_IVIFN_Algebraic_Plus,Interaction_IVIFN_Einstein_Multiply,Interaction_IVIFN_Einstein_Plus,
+#                                 Interaction_PFN_Algebraic_Multiply,Interaction_PFN_Algebraic_Plus,Interaction_PFN_Einstein_Multiply,Interaction_PFN_Einstein_Plus,
+#                                 Interaction_IVPFN_Algebraic_Multiply,Interaction_IVPFN_Algebraic_Plus,Interaction_IVPFN_Einstein_Multiply,Interaction_IVPFN_Einstein_Plus,
+#                                 Interaction_FFN_Algebraic_Multiply,Interaction_FFN_Algebraic_Plus,Interaction_FFN_Einstein_Multiply,Interaction_FFN_Einstein_Plus,
+#                                 Interaction_IVFFN_Algebraic_Multiply,Interaction_IVFFN_Algebraic_Plus,Interaction_IVFFN_Einstein_Multiply,Interaction_IVFFN_Einstein_Plus,)
